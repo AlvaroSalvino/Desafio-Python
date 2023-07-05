1d844cb2bbe720e3fe3c91d1111330d1806756e0
Alvaro <alvaro.salvino@gmail.com>
2023-07-05 20:30:25 -0300

    Update Desafio_Python.py

--- Desafio_Python.py
+++ Desafio_Python.py
@@ -1,34 +1,63 @@
-deposito = {
-}
-saque = {
-}
-extrato = {
-}
+menu = """
 
+1 DEPOSITAR
+
+2 SACAR
+
+3 EXTRATO
+
+0 SAIR
+
+"""
+
+saldo = 0
+limite = 500
+extrato = ""
+numeroSaque = 0
+LIMIE_SAQUES = 3
 
 while True:
-    opcao = input(f'''========== Banco DIO ==========
+    opcao  = input(menu)
+
+    if opcao == "1":
+        valor = float(input("Quanto deseja Depositar?: "))
         
-        1 - Depositar
+        if valor > 0:
+            saldo += valor
+            extrato += f"Depósito: R$ {valor:.2f}\n"
 
-        2 - Sacar
+        else:
+            print("Opção inválida, informe valor positivo")
+    
+    elif opcao == "2":
+        valor = float(input("Quanto deseja sacar?: "))
 
-        3 - Ver Extrato
+        excedeuSaldo = valor > saldo
 
-        0 - Sair
-                
-        INSIRA UMA OPÇÃO: ''')
+        excedeuLimite = valor > limite
 
-    if opcao == '1':
-        while True:
-            deposito = int(input('Quanto quer depositar?: '))
-            if deposito <= 0:
-                print('Não é possível depositar valores negativos, por favor insira um valor válido!')
-            else:
-                break
-    if opcao == '2':
-        saque = int(input('Quanto deseja sacar?: '))
-        if saque > '500':
-            print('Limite máximo para saque: R$ 500')
-    if opcao == '3':
-        print(f'''Seu extrato:''')
+        excedeuSaques = numeroSaque >= LIMIE_SAQUES
+
+        if excedeuSaldo:
+            print("Você não tem saldo suficiente!")
+
+        elif excedeuLimite:
+            print("Você não tem saldo suficiente para este saque!")
+
+        elif excedeuSaques:
+            print("Você não tem saldo de saques suficiente! LIMITE = 3/dia")
+
+        else:
+            print("VALOR INVALIDO!")
+
+    
+    elif opcao == "3":
+        print("##ESTRATO##")
+        print("Não há movimentação." if not extrato else extrato)
+        print(f"Saldo: R$ {saldo:.2f}")
+
+    elif opcao == "0":
+        break
+
+    else:
+        print("Opção inválida, favor selecionar opção válida.")
